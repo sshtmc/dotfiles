b650a8933a8161b05a731294da257f367ac2be4f
Saša Tomić <sasa.git@kalaj.org>
2024-01-28 14:53:24 +0100

    Add .config/lvim/config.lua Add .config/lvim/lazy-lock.json

---
+++ private_dot_config/lvim/config.lua
@@ -0,0 +1,16 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.plugins = {
+  { "github/copilot.vim" },
+  { "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup({
+      log_level = "error",
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+    })
+    end,
+  },
+}
